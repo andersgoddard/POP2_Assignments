c8f844cab717ba194a6815d4e12f5038002dae0b
Andrew Goddard <andersgoddard@gmail.com>
2021-02-11 06:46:30 +0000

    The original algorithm failed on test cases with the same number of letters because the else if clause for when the targetWordBuilder only contained 1 letter contained a bug - it returned the letter in the target word, even if the letter in the check word was later in the alphabet. Actually, the logic to return the correct letter was exactly the same as the else clause, so the else if clause wasn't necessary either. Galling. Strapping on the cilice now.

--- Wk_3/Anagrams/Anagrams.java
+++ Wk_3/Anagrams/Anagrams.java
@@ -1,3 +1,6 @@
+// 13184648
+// agodda02@mail.bbk.ac.uk
+
 import java.io.*;
 import java.util.*;
 
@@ -14,6 +17,7 @@ public class Anagrams {
 		int unmatchedLetterIndex = -1;
 		char letter;
 		String alphabet = "abcdefghijklmnopqrstuvwxyz";
+		ArrayList<Character> differences = new ArrayList<Character>();
 
 		
 		if (a.length() <= b.length()){
@@ -21,39 +25,43 @@ public class Anagrams {
 			targetWord = b;
 		} else {
 			checkWord = b;
-			targetWord = a;
+			targetWord = a; 
 		}
-				
+		
 		for (int i = 0; i < targetWord.length(); i++){
-			letter = Character.toLowerCase(targetWord.charAt(i));
-			alphabetIndex = alphabet.indexOf(letter);
+			letter = Character.toLowerCase(targetWord.charAt(i));        
+			alphabetIndex = alphabet.indexOf(letter);					
 			if (alphabetIndex != -1) targetWordBuilder.append(letter);
 		}
+		
 		
 		for (int i = 0; i < checkWord.length(); i++){
 			letter = Character.toLowerCase(checkWord.charAt(i));
 			alphabetIndex = alphabet.indexOf(letter);
+
 			if (alphabetIndex != -1) {
-				targetIndex = targetWordBuilder.indexOf(Character.toString(letter));
-				if (targetIndex != -1) targetWordBuilder.deleteCharAt(targetIndex);
-				if (alphabetIndex > unmatchedLetterIndex) unmatchedLetterIndex = alphabetIndex; 
+				targetIndex = targetWordBuilder.indexOf(Character.toString(letter)); 
+				if (targetIndex != -1) targetWordBuilder.deleteCharAt(targetIndex);  
+				else if (alphabetIndex > unmatchedLetterIndex){						 
+					unmatchedLetterIndex = alphabetIndex; 							 
+				}
 			}
 		}
 		
 		for (int i = 0; i < targetWordBuilder.length(); i++){
 			letter = targetWordBuilder.charAt(i);
 			int index = alphabet.indexOf(letter);
-			if (index > unmatchedLetterIndex) unmatchedLetterIndex = index;
+			if (index > unmatchedLetterIndex)
+				unmatchedLetterIndex = index;
 		}
-		
+			
 		if (targetWordBuilder.length() == 0) return '!';
-		else if (targetWordBuilder.length() == 1) return targetWordBuilder.charAt(0);
 		else return alphabet.charAt(unmatchedLetterIndex);
 		
 	}
 
     public static void main( String[] args ) 
     { 
-    
+		
     }
 }
